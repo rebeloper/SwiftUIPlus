d0df9c509f5b1e5c4332c8e99fa58aa4a50a61d2
Alex Nagy <rebeloper@users.noreply.github.com>
2021-02-28 15:21:54 +0200

    AVPlayerView

---
+++ Sources/SwiftUIPlus/UIViewControllerRepresentables/AVPlayerView.swift
@@ -0,0 +1,32 @@
+//
+//  AVPlayerView.swift
+//  
+//
+//  Created by Alex Nagy on 28.02.2021.
+//
+
+import SwiftUI
+import AVKit
+
+public struct AVPlayerView: UIViewControllerRepresentable {
+    public typealias UIViewControllerType = AVPlayerViewController
+    
+    public var url: URL?
+    public var shouldAutoPlay: Bool
+    
+    public func makeUIViewController(context: Context) -> AVPlayerViewController {
+        let controller = AVPlayerViewController()
+        if let url = url {
+            let player = AVPlayer(url: url)
+            controller.player = player
+            if shouldAutoPlay {
+                player.play()
+            }
+        }
+        return controller
+    }
+    
+    public func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) { }
+}
+
+
