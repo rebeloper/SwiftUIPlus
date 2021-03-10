ac85a2a405c7990df1776fda3cdca6041d5ad607
Alex Nagy <therebeloper@gmail.com>
2021-03-10 17:49:04 +0200

    Page

--- Sources/SwiftUIPlus/Navigation/Page.swift
+++ Sources/SwiftUIPlus/Navigation/Page.swift
@@ -12,48 +12,48 @@ public struct Page<Destination: View, Label: View>: View {
     @State private var isActive = false
     
     private var pageType: PageType
-    private let destination: () -> Destination
+    private let destination: Destination
     private let onDismiss: (() -> Void)?
-    private let label: () -> Label
+    private let label: Label
     
-    /// NavigationLink that controls a navigation presentation when a given condition is true.
+    /// Button that controls a navigation presentation when a given condition is true.
     /// - Parameters:
     ///   - pageType: The page type presented. Default is .push.
     ///   - destination: A closure returning the content of the destination.
     ///   - onDismiss: A closure executed when the push dismisses.
     ///   - label: A view that is embeded into a NavigationLink.
     public init(_ pageType: PageType = .push,
-                destination: @escaping () -> Destination,
+                @ViewBuilder destination: () -> Destination,
                 onDismiss: (() -> Void)? = nil,
-                label: @escaping () -> Label) {
+                @ViewBuilder label: () -> Label) {
         self.pageType = pageType
-        self.destination = destination
+        self.destination = destination()
         self.onDismiss = onDismiss
-        self.label = label
+        self.label = label()
     }
     
     public var body: some View {
         Group {
             switch pageType {
             case .push:
-                NavigationLink(destination: destination().onDisappear(perform: {
+                NavigationLink(destination: destination.onDisappear(perform: {
                     onDismiss?()
                 })) {
-                    label()
+                    label
                 }
             case .sheet:
                 Button {} label: {
-                    label()
+                    label
                 }
                 .sheet(isPresented: $isActive, onDismiss: onDismiss) {
-                    destination()
+                    destination
                 }
             case .fullScreenSheet:
                 Button {} label: {
-                    label()
+                    label
                 }
                 .fullScreenCover(isPresented: $isActive, onDismiss: onDismiss) {
-                    destination()
+                    destination
                 }
             }
         }
