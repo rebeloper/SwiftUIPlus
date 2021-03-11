90d7990e9f46de9389a66814d82c2c7bc50d0d09
Alex Nagy <therebeloper@gmail.com>
2021-03-11 23:36:37 +0200

    TabViewMananger

--- Sources/SwiftUIPlus/TabViewMananger/TabViewMananger.swift
+++ Sources/SwiftUIPlus/TabViewMananger/TabViewMananger.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 public class TabViewMananger: ObservableObject {
+    
+    /// TabView selection
     @Published public var selection: Int = 0
     
+    /// Creates a TabView mananger
+    /// - Parameter initialSelection: the initial TabView selection
     public init(initialSelection: Int) {
         self.selection = initialSelection
     }
 }
 
 public extension View {
+    /// Adds a TabViewMananger to the view
+    /// - Parameter tabViewMananger: TabView Mananger
+    /// - Returns: a view with a TabViewMananger
     func uses(_ tabViewMananger: TabViewMananger) -> some View {
         self.environmentObject(tabViewMananger)
     }
