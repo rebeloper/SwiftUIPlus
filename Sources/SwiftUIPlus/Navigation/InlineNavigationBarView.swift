c8b9e1fc9f1087b76cd79cdbd583e34540627b8a
Alex Nagy <therebeloper@gmail.com>
2021-03-11 15:20:28 +0200

    shows divider

--- Sources/SwiftUIPlus/Navigation/InlineNavigationBarView.swift
+++ Sources/SwiftUIPlus/Navigation/InlineNavigationBarView.swift
@@ -12,6 +12,7 @@ public struct InlineNavigationBarView<TitleView: View, LeadingView: View, Traili
     public let leadingView: LeadingView
     public let trailingView: TrailingView
     public let backgroundView: BackgroundView
+    public let showsDivider: Bool
     public let content: Content
     
     public var body: some View {
@@ -33,7 +34,9 @@ public struct InlineNavigationBarView<TitleView: View, LeadingView: View, Traili
                 .padding(.horizontal, 12)
                 .padding(.vertical, 8)
                 .background(backgroundView)
-                Divider().ignoresSafeArea()
+                if showsDivider {
+                    Divider().ignoresSafeArea()
+                }
                 Spacer().frame(height: 0)
                 content
             }
@@ -41,6 +44,20 @@ public struct InlineNavigationBarView<TitleView: View, LeadingView: View, Traili
         }
     }
 }
+
+public struct InlineNavigationBarViewModifier<TitleView: View, LeadingView: View, TrailingView: View, BackgroundView: View>: ViewModifier {
+    
+    public var titleView: () -> TitleView
+    public var leadingView: () -> LeadingView
+    public var trailingView: () -> TrailingView
+    public var backgroundView: () -> BackgroundView
+    public var showsDivider: Bool
+    
+    public func body(content: Content) -> some View {
+        InlineNavigationBarView(titleView: titleView(), leadingView: leadingView(), trailingView: trailingView(), backgroundView: backgroundView(), showsDivider: showsDivider, content: content)
+    }
+}
+
 
 public extension View {
     /// Inline navigation bar
@@ -50,7 +67,7 @@ public extension View {
     ///   - trailingView: A view at the trailing side of the navigation bar
     ///   - backgroundView: A view that is the background of the navigation bar
     /// - Returns: Inline navigation bar
-    func inlineNavigationBar<TitleView: View, LeadingView: View, TrailingView: View, BackgroundView: View>(titleView: TitleView, leadingView: LeadingView, trailingView: TrailingView, backgroundView: BackgroundView) -> some View {
+    func inlineNavigationBar<TitleView: View, LeadingView: View, TrailingView: View, BackgroundView: View>(titleView: TitleView, leadingView: LeadingView, trailingView: TrailingView, backgroundView: BackgroundView, showsDivider: Bool = true) -> some View {
         modifier(InlineNavigationBarViewModifier(titleView: {
             titleView
         }, leadingView: {
@@ -59,6 +76,6 @@ public extension View {
             trailingView
         }, backgroundView: {
             backgroundView
-        }))
+        }, showsDivider: showsDivider))
     }
 }
