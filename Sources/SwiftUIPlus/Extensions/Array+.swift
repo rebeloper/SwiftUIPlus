88dc02ad22275a059804306740519262a5f71f57
Alex Nagy <therebeloper@gmail.com>
2021-03-14 13:32:03 +0200

    Array+

---
+++ Sources/SwiftUIPlus/Extensions/Array+.swift
@@ -0,0 +1,29 @@
+//
+//  Array+.swift
+//  
+//
+//  Created by Alex Nagy on 14.03.2021.
+//
+
+import Foundation
+
+/// Makes `Array` `Codable` to be able to use arrays with @AppStorage
+extension Array: RawRepresentable where Element: Codable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode([Element].self, from: data)
+        else {
+            return nil
+        }
+        self = result
+    }
+
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+              let result = String(data: data, encoding: .utf8)
+        else {
+            return "[]"
+        }
+        return result
+    }
+}
