2006b7ba905618c6f935bbc4430f60246c45780a
Alex Nagy <therebeloper@gmail.com>
2021-03-15 17:44:37 +0200

    FlexibleSheet

--- Sources/SwiftUIPlus/Navigation/FlexibleSheet.swift
+++ Sources/SwiftUIPlus/Navigation/FlexibleSheet.swift
@@ -7,48 +7,132 @@
 
 import SwiftUI
 
-public struct FlexibleSheet<Destination: View, Label: View>: View {
+public extension FlexibleSheet where Label == EmptyView {
     
-    private var flexibleSheetManager: FlexibleSheetManager
-    private let swipesToDismiss: Bool
-    private let ignoresSafeArea: Bool
-    private let destination: () -> Destination
-    private let onDismiss: () -> ()
-    private let label: () -> Label
-    
-    /// Button that presents a sheet.
+    /// `EmptyView` with `isActive` `Binding<Bool>` that presents a `Destination` view when `isActive` is set to `true`.
     /// - Parameters:
-    ///   - flexibleSheetManager: a FlexibleSheetManager.
-    ///   - swipesToDismiss: Should the sheet be able to be dismissed with a swipe.
-    ///   - ignoresSafeArea: Should the sheet content ignore the safe area.
-    ///   - destination: A closure returning the content of the sheet.
-    ///   - onDismiss: A closure executed when the sheet dismisses.
-    ///   - label: A view that is embeded into a Button.
-    public init(_ flexibleSheetManager: FlexibleSheetManager,
-                swipesToDismiss: Bool = true,
-                ignoresSafeArea: Bool = false,
-                destination: @escaping () -> Destination,
-                onDismiss: @escaping () -> () = {},
-                label: @escaping () -> Label) {
+    ///   - type: The page type presented. Default is .push.
+    ///   - isActive: A binding string whether the destination is presented.
+    ///   - destination: A closure returning the content of the destination.
+    ///   - onDismiss: A closure executed when the navigation dismisses the presented view.
+    init(_ flexibleSheetManager: FlexibleSheetManager,
+         isActive: Binding<Bool>,
+         swipesToDismiss: Bool = true,
+         ignoresSafeArea: Bool = false,
+         destination: @escaping () -> Destination,
+         onDismiss: @escaping () -> () = {}) {
         self.flexibleSheetManager = flexibleSheetManager
+        self.flexibleSheetStyle = .emptyView
+        self._isActiveBinding = isActive
         self.swipesToDismiss = swipesToDismiss
         self.ignoresSafeArea = ignoresSafeArea
         self.destination = destination
+        self.label = { EmptyView() }
         self.onDismiss = onDismiss
+    }
+}
+
+public struct FlexibleSheet<Destination: View, Label: View>: View {
+    
+    @State private var isActive = false
+    @State private var currentIsActive: Bool = false
+    
+    private var flexibleSheetManager: FlexibleSheetManager
+    private var flexibleSheetStyle: FlexibleSheetStyle
+    @Binding private var isActiveBinding: Bool
+    private let swipesToDismiss: Bool
+    private let ignoresSafeArea: Bool
+    private let destination: () -> Destination
+    private let label: () -> Label
+    private let onDismiss: () -> ()
+    
+    public init(_ flexibleSheetManager: FlexibleSheetManager,
+                style: FlexibleSheetStyle = .button,
+                swipesToDismiss: Bool = true,
+                ignoresSafeArea: Bool = false,
+                destination: @escaping () -> Destination,
+                label: @escaping () -> Label,
+                onDismiss: @escaping () -> () = {}) {
+        self.flexibleSheetManager = flexibleSheetManager
+        self.flexibleSheetStyle = style
+        self._isActiveBinding = .constant(false)
+        self.swipesToDismiss = swipesToDismiss
+        self.ignoresSafeArea = ignoresSafeArea
+        self.destination = destination
         self.label = label
+        self.onDismiss = onDismiss
     }
     
     public var body: some View {
-        Button {
-            flexibleSheetManager.present(destination: {
-                destination().anyView()
-            }, onDismiss: {
-                onDismiss()
-            }, config: flexibleSheetManager.config)
-        } label: {
-            label()
+        VStack {
+            switch flexibleSheetStyle {
+            case .button:
+                Button {
+                    flexibleSheetManager.present(destination: {
+                        destination().anyView()
+                    }, onDismiss: {
+                        onDismiss()
+                    }, config: flexibleSheetManager.config)
+                } label: {
+                    label()
+                }
+            case .view:
+                label()
+                    .onTapGesture {
+                        isActive.toggle()
+                    }
+                    .onReceive([isActive].publisher, perform: { isActive in
+                        if currentIsActive == isActive { return }
+                        currentIsActive = isActive
+                        if isActive {
+                            flexibleSheetManager.present(destination: {
+                                destination().anyView()
+                            }, onDismiss: {
+                                currentIsActive = false
+                                onDismiss()
+                            }, config: flexibleSheetManager.config)
+                        } else {
+                            flexibleSheetManager.dismiss()
+                        }
+                    })
+                    .onReceive(flexibleSheetManager.$isActive) { (isActive) in
+                        if !isActive {
+                            self.isActive = isActive
+                        }
+                    }
+            case .emptyView:
+                Button(action: {}, label: {
+                    EmptyView()
+                })
+                .onReceive([isActiveBinding].publisher, perform: { isActive in
+                    if currentIsActive == isActive { return }
+                    currentIsActive = isActive
+                    if isActive {
+                        flexibleSheetManager.present(destination: {
+                            destination().anyView()
+                        }, onDismiss: {
+                            currentIsActive = false
+                            onDismiss()
+                        }, config: flexibleSheetManager.config)
+                    } else {
+                        flexibleSheetManager.dismiss()
+                    }
+                })
+                .onReceive(flexibleSheetManager.$isActive) { (isActive) in
+                    if !isActive {
+                        self.isActiveBinding = isActive
+                    }
+                }
+            }
         }
+        
     }
+}
+
+public enum FlexibleSheetStyle {
+    case button
+    case view
+    case emptyView
 }
 
 
