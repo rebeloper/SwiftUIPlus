fa65053822cf156240234284cd2f20be817c9a48
Alex Nagy <therebeloper@gmail.com>
2021-03-30 10:34:38 +0300

    border

--- Sources/SwiftUIPlus/Extensions/View+.swift
+++ Sources/SwiftUIPlus/Extensions/View+.swift
@@ -100,3 +100,17 @@ public extension View {
         }
     }
 }
+
+public extension View {
+    /// Adds a border to the view
+    /// - Parameters:
+    ///   - content: the view
+    ///   - width: border width
+    ///   - cornerRadius: bodred corner radius
+    /// - Returns: a view with a border and corner radius
+    func border<S: ShapeStyle>(_ content: S, width: CGFloat = 1, cornerRadius: CGFloat = 15) -> some View {
+         let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
+         return clipShape(roundedRect)
+              .overlay(roundedRect.strokeBorder(content, lineWidth: width))
+     }
+ }
