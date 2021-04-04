f2d6a006ddd8a286c543e061f5005804882f8093
Alex Nagy <therebeloper@gmail.com>
2021-04-04 13:38:32 +0300

    statusBarHeight

---
+++ Sources/SwiftUIPlus/Utils/StatusBar.swift
@@ -0,0 +1,19 @@
+//
+//  StatusBar.swift
+//  
+//
+//  Created by Alex Nagy on 04.04.2021.
+//
+
+import UIKit
+
+public func statusBarHeight() -> CGFloat {
+    var statusBarHeight: CGFloat = 0
+    if #available(iOS 13.0, *) {
+        let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+        statusBarHeight = window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+    } else {
+        statusBarHeight = UIApplication.shared.statusBarFrame.height
+    }
+    return statusBarHeight
+}
