7ae24919cc07b11926107f04b01c1786a3c66fd1
Alex Nagy <therebeloper@gmail.com>
2021-04-15 15:35:17 +0300

    Page docs

--- Sources/SwiftUIPlus/Navigation/Page.swift
+++ Sources/SwiftUIPlus/Navigation/Page.swift
@@ -28,6 +28,81 @@ public extension Page where Label == EmptyView {
     }
 }
 
+/// `Page` is a `View`  that when tapped presents a `Destination` view. It can also be an `EmptyView` with `isActive` `Binding<Bool>` that presents a `Destination` view when `isActive` is set to `true`.
+///
+/// Navigation is `SwiftUI` is handeled by multiple items (ex. NavigationLink, .sheet, .fullScreenCover). `Page` brings them all into one convenient syntax.
+/// `Page` behaves much like a `NavigationLink`, but with added extras. It has two initializers: one has a label view, the other does not have a label property because it is set to `EmptyView` by default.
+/// The two case are:
+///     - when you want to navigate to another Page upon a user initiated tap on a view
+///     - when you want to navigate to another Page but no tappable view should be available on the screen
+///
+/// Let's take a look at some examples:
+///
+/// Your root view has to be inside a `NavigationView`:
+///
+/// ```
+/// NavigationView {
+///     ContentView()
+/// }
+/// ```
+///
+/// Than inside `ContentView` you can create a `Page` that will navigate to the `DetailView`:
+///
+/// ```
+/// Page {
+///     DetailView()
+/// } label: {
+///     Text("Go to DetailView")
+/// }
+/// ```
+///
+/// The same outcome can be achieved with the other initializer:
+///
+/// ```
+/// @State private var isDetailViewActive = false // declared outside of the body
+/// ```
+/// ```
+/// Button {
+///     isDetailViewActive.toggle()
+/// } label: {
+///     Text("Go to DetailView")
+/// }
+/// Page(isActive: $isDetailViewActive) {
+///     DetailView()
+/// }
+/// ```
+///
+/// You can also choose the type of label you want to see, default is `.button` that behaves as a `Button`. Options are: `.button`, `.view` and `.emptyView`.
+///
+/// ```
+/// Page(.view) {
+///     DetailView()
+/// } label: {
+///     Text("Go to DetailView")
+/// }
+/// ```
+///
+/// There's an option to listen to `onDismiss` events trigerred when the Page is dismissed:
+///
+/// ```
+/// Page(.view) {
+///     DetailView()
+/// } label: {
+///     Text("Go to DetailView")
+/// } onDismiss: {
+///     print("DetailView was dismissed")
+/// }
+/// ```
+/// Also you may choose the type of navigation, default is `push`. Options are: `push`, `sheet` and `fullScreenSheet`.
+///
+/// ```
+/// Page(type: .sheet) {
+///     DetailView()
+/// } label: {
+///     Text("Go to DetailView")
+/// }
+/// ```
+///
 public struct Page<Destination: View, Label: View>: View {
     
     @State private var isActive = false
