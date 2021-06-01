16cb77e72f13cd393ae09091d2c896df7f54ca31
Alex Nagy <therebeloper@gmail.com>
2021-06-01 13:31:59 +0300

    Custom error

---
+++ Sources/SwiftUIPlus/CustomError/CustomError.swift
@@ -0,0 +1,40 @@
+//
+//  CustomError.swift
+//  
+//
+//  Created by Alex Nagy on 01.06.2021.
+//
+
+import Foundation
+
+public enum CustomError: Error {
+    case with(description: String, code: Int)
+}
+
+extension CustomError {
+    public var code: Int {
+        switch self {
+        case .with(description: _, code: let code):
+            return code
+        }
+    }
+}
+
+extension CustomError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .with(description: let description, code: _):
+            return description
+        }
+    }
+}
+
+extension CustomError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .with(description: let description, code: _):
+            return NSLocalizedString(description, comment: description)
+        }
+    }
+}
+
