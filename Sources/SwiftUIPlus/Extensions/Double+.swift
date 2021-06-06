66daf921ddf4699f49a5b0053f300d07225188c7
Alex Nagy <therebeloper@gmail.com>
2021-06-06 13:57:33 +0300

    Double+

--- Sources/SwiftUIPlus/Extensions/Double+.swift
+++ Sources/SwiftUIPlus/Extensions/Double+.swift
@@ -9,8 +9,13 @@ import Foundation
 
 public extension Double {
     /// Rounds the double to decimal places value
-    func rounded(toPlaces places:Int) -> Double {
+    func rounded(toPlaces places: Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded() / divisor
+    }
+    
+    /// gives back a String with decimal with places value
+    func toDecimalString(withPlaces places: Int) -> String {
+        String(format: "%.\(places)f", self)
     }
 }
