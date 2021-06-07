ebeaa76188ada8e183201fb6f93bfdfd8d588653
Alex Nagy <therebeloper@gmail.com>
2021-06-07 12:00:22 +0300

    ViewError

--- Sources/SwiftUIPlus/ViewError/ViewError.swift
+++ Sources/SwiftUIPlus/ViewError/ViewError.swift
@@ -10,18 +10,16 @@ import SwiftUI
 public struct ViewError: Equatable {
     
     public var error: Error
-    @State public var isActive: Bool = false
     
     public init(error: Error = CustomError.initial()) {
         self.error = error
     }
     
     public static func == (lhs: ViewError, rhs: ViewError) -> Bool {
-        return lhs.error.localizedDescription == rhs.error.localizedDescription && lhs.isActive == rhs.isActive
+        return lhs.error.localizedDescription == rhs.error.localizedDescription
     }
     
     public mutating func activate(with error: Error) {
         self.error = error
-        self.isActive.toggle()
     }
 }
