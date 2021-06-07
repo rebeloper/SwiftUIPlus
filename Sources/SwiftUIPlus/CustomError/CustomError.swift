1c8bed9684a46637f489e072412a84a624764989
Alex Nagy <therebeloper@gmail.com>
2021-06-07 11:08:06 +0300

    public

--- Sources/SwiftUIPlus/CustomError/CustomError.swift
+++ Sources/SwiftUIPlus/CustomError/CustomError.swift
@@ -39,7 +39,7 @@ extension CustomError: LocalizedError {
 }
 
 extension CustomError {
-    func initial() -> CustomError {
+    public func initial() -> CustomError {
         .with(description: "", code: 0)
     }
 }
